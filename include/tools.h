0e7400e3370e13f3f6b0f79e45ce452bf433b2cf
Fabian Lippold <fabianlippold1184@gmail.com>
2022-10-12 23:51:33 +0200

    update

--- include/tools.h
+++ include/tools.h
@@ -1,6 +1,8 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+#include <stdio.h>
+
 #define CHECK(f, e, ...) if(f) { \
     fprintf(stderr, "Error in %s, line %d: %s\n", __func__, __LINE__, e == NULL ? "" : e(__VA_ARGS__)); \
     }
